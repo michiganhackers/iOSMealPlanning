34cae39b74869c38a2e8a829145b2c031f332478
drewskis <drewskis@umich.edu>
2021-10-27 17:14:48 -0400

    Initial Commit

---
+++ MHacksMealPlanningApp/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesViewController.swift
+//  MHacksMealPlanningApp
+//
+//  Created by the Michigan Hanckers iOS Team on 10/27/21.
+//
+
+import Foundation
+
+struct K {
+    struct Segues {
+        let loginSegueID = "LoginToHome"
+        let registerSegueID = "RegisterToHome"
+    }
+    
+    let theBestSlurpeeFlavor = "Coke" //Non-negotiable
+}
---
+++ MHacksMealPlanningApp/View Controllers/StartupScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RecipesViewController.swift
+//  MHacksMealPlanningApp
+//
+//  Created by the Michigan Hanckers iOS Team on 10/27/21.
+//
+
+import UIKit
+
+class StartupScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
